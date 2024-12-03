1e78a4f1a40e9acdcd3c64067a1a072d845152c7
okulapik <okumuralapis.makarenko@gmail.com>
2024-12-03 18:21:16 +0400

    first change

---
+++ main.py
@@ -0,0 +1,31 @@
+import sys
+import sqlite3
+from PyQt6.QtWidgets import QApplication, QMainWindow, QTableWidget, QTableWidgetItem
+from PyQt6 import uic
+
+
+class Coffee(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('main.ui', self)
+        self.con = sqlite3.connect('coffee.sqlite')
+        self.cur = self.con.cursor()
+        self.loadtable()
+
+    def loadtable(self):
+        res = self.cur.execute("SELECT * FROM Coffee").fetchall()
+        self.table.setColumnCount(len(res))
+        self.table.setRowCount(len(res[0]))
+        for i, row in enumerate(res):
+            self.table.setRowCount(
+                self.table.rowCount() + 1)
+            for j, elem in enumerate(row):
+                self.table.setItem(
+                    i, j, QTableWidgetItem(str(elem)))
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Coffee()
+    ex.show()
+    sys.exit(app.exec())
